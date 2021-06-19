17fd731399a8fda20ffa6aa7a9f55af13e95ab61
ariful4321 <79929704+ariful4321@users.noreply.github.com>
2021-06-19 22:49:01 +0600

    Add files via upload

---
+++ A2.php
@@ -0,0 +1,14 @@
+<?php
+	if(isset($_REQUEST['submit'])){
+		$gender	=  $_REQUEST['gender'];
+		if($gender == ""){
+			echo "Null input";
+		}
+        else{
+			echo $gender;
+		}
+	}else{
+		echo 'invalid input';
+	}
+
+?>
